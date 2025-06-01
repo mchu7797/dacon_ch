5d96e7a5db03c5c8eaa3ed73ceb4fa88c7b40381
Minseok Chu <mchu7797@gmail.com>
2025-06-01 14:35:43 +0900

    세부 설정 조정

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ class Config:
     early_stopping_patience: int = 5
     early_stopping_minimum_delta: float = 0.0002
 
-    use_tta: bool = False
+    use_tta: bool = True
     tta_batch_size_divisor: int = 4
 
     image_size: int = 224
-    batch_size: int = 256
-    epochs: int = 15
+    batch_size: int = 128
+    epochs: int = 25
     learning_rate: float = 3e-4
     seed: int = 42
 
